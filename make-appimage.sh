a818703d8a442f3604032ece5ab88ac6db5fdac8
Samuel <36420837+Samueru-sama@users.noreply.github.com>
2026-01-03 01:45:38 -0400

    mmm

--- make-appimage.sh
+++ make-appimage.sh
@@ -35,12 +35,17 @@ kek=.$(tr -dc 'A-Za-z0-9_=-' < /dev/urandom | head -c 10)
 rm -f ./AppDir/bin/crun                 ./AppDir/shared/bin/crun
 cp -v /usr/bin/crun                     ./AppDir/bin/crun.wrapped
 patchelf --set-interpreter /tmp/"$kek"  ./AppDir/bin/crun.wrapped
-cat <<EOF > ./AppDir/bin/crun
+cat <<'EOF' > ./AppDir/bin/crun
 #!/bin/sh
-cp -f "\$APPDIR"/shared/lib/ld-linux*.so* /tmp/"$kek"
-export LD_LIBRARY_PATH="\$APPDIR"/shared/lib
-exec "\$APPDIR"/sharun crun.wrapped "\$@"
+
+[ -n "$APPDIR" ] || APPDIR=$(cd "${0%/*}"/../ && echo "$PWD")
+export PATH="$PATH:$APPDIR/bin:/usr/bin:/bin:/usr/sbin:/usr/sbin"
+export LD_LIBRARY_PATH="$APPDIR"/shared/lib
+
+cp -f "$APPDIR"/shared/lib/ld-linux*.so* /tmp/"$kek"
+exec "$APPDIR"/sharun crun.wrapped "$@"
 EOF
+sed -i -e "s|\$kek|$kek|g" ./AppDir/bin/crun
 chmod +x ./AppDir/bin/crun*
 
 # Turn AppDir into AppImage
