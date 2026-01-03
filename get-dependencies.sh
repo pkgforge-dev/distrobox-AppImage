00a4c9b9e0c91d0b6eb2a0d910e5d9dfca4608b4
Samuel <36420837+Samueru-sama@users.noreply.github.com>
2026-01-03 00:47:41 -0400

    oops

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -6,7 +6,7 @@ ARCH=$(uname -m)
 
 echo "Installing package dependencies..."
 echo "---------------------------------------------------------------"
-pacman -Syu --noconfirm podman
+pacman -Syu --noconfirm distrobox podman
 
 #echo "Installing debloated packages..."
 #echo "---------------------------------------------------------------"
