d4cefa40d3bd025b81fb08ae970e522eee1f5cff
Samuel <36420837+Samueru-sama@users.noreply.github.com>
2026-01-03 00:41:07 -0400

    super broken

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -6,13 +6,13 @@ ARCH=$(uname -m)
 
 echo "Installing package dependencies..."
 echo "---------------------------------------------------------------"
-pacman -Syu --noconfirm distrobox
+pacman -Syu --noconfirm podman
 
 #echo "Installing debloated packages..."
 #echo "---------------------------------------------------------------"
 #get-debloated-pkgs --add-common --prefer-nano
 
 # Comment this out if you need an AUR package
-make-aur-package lilipod
+#make-aur-package
 
 # If the application needs to be manually built that has to be done down here
