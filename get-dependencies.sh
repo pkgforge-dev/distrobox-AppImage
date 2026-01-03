3146d81bd9ef33bcbb4735708cafa6a8c6151b20
Samuel <36420837+Samueru-sama@users.noreply.github.com>
2026-01-03 01:13:37 -0400

    add `patchelf`

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -6,7 +6,7 @@ ARCH=$(uname -m)
 
 echo "Installing package dependencies..."
 echo "---------------------------------------------------------------"
-pacman -Syu --noconfirm distrobox podman
+pacman -Syu --noconfirm distrobox podman patchelf
 
 #echo "Installing debloated packages..."
 #echo "---------------------------------------------------------------"
